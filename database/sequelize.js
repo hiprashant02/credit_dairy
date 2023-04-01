18f38828c785970b4eb9d641ddd19caecc616d5c
hiprashant02 <51109729+hiprashant02@users.noreply.github.com>
2023-04-01 22:49:19 +0530

    Update sequelize.js

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,11 +1,9 @@
 
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize(
-   'credit_dairy', 
-   'root', 
-   '', {
-      dialect: 'mysql',
-      host: 'localhost'
-   }
-);
+const sequelize = new Sequelize('credit_dairy', 'root', '', {
+  host: 'localhost',
+  port: 3306, // This is the default port number for MySQL
+  dialect: 'mysql'
+});
+
 module.exports = sequelize
