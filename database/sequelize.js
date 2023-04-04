7ac641aa6eb8ab6f348e3e574191d09505a2a1f4
hiprashant02 <51109729+hiprashant02@users.noreply.github.com>
2023-04-04 16:46:13 +0530

    direct path update

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,18 +1,21 @@
 
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize('credit_dairy', 'root', '', {
-host: 'localhost',
-port: 3306,
-dialect: 'mysql'
+  host: '127.0.0.1',
+  dialect: 'mysql',
+  dialectOptions: {
+    socketPath: '/var/run/mysqld/mysqld.sock'
+  }
 });
 
 
+
 sequelize.authenticate()
-   .then(() => {
-      console.log('\n\n\nConnection has been established successfully.\n\n\n');
-   })
-   .catch(err => {
-      throw err;
-   });
+  .then(() => {
+    console.log('\n\n\nConnection has been established successfully.\n\n\n');
+  })
+  .catch(err => {
+    throw err;
+  });
 
 module.exports = sequelize
