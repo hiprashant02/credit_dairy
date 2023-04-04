a11880b20b00d2f19c59f0c390f74186297ba415
hiprashant02 <51109729+hiprashant02@users.noreply.github.com>
2023-04-04 14:36:17 +0530

    throw err

--- database/sequelize.js
+++ database/sequelize.js
@@ -14,8 +14,7 @@ sequelize.authenticate()
       console.log('\n\n\nConnection has been established successfully.\n\n\n');
    })
    .catch(err => {
-      console.error('\n\n\nUnable to connect to the database:', err);
-      console.error('\n\n\n')
+      throw err;
    });
 
 module.exports = sequelize
