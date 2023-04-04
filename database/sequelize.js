9728d13c4e599b32f682edce4c7d6e276332aa3c
hiprashant02 <51109729+hiprashant02@users.noreply.github.com>
2023-04-04 14:45:27 +0530

    new seq

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,14 +1,11 @@
 
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize(
-   'credit_dairy',
-   'root',
-   '', {
-   dialect: 'mysql',
-   host: 'localhost',
-   port: 3306,
-}
-);
+const sequelize = new Sequelize('credit_dairy', 'root', '', {
+  host: 'localhost',
+  port: 3306,
+  dialect: 'mysql'
+});
+
 sequelize.authenticate()
    .then(() => {
       console.log('\n\n\nConnection has been established successfully.\n\n\n');
