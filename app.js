13d2940578a551fdc7031a0458512e8f38bb3422
hiprashant02 <51109729+hiprashant02@users.noreply.github.com>
2023-04-04 14:16:41 +0530

    port variable missing error fixed

--- app.js
+++ app.js
@@ -80,5 +80,5 @@ app.use(function(err, req, res, next) {
 });
 
 app.listen(8080, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${8080}`)
 })
