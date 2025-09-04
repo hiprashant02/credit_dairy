dc89c48df5cd53c969d1f271e51675a1ee68f4e4
hiprashant02 <51109729+hiprashant02@users.noreply.github.com>
2025-09-04 09:59:58 +0530

    Update send_otp.js

--- utils/send_otp.js
+++ utils/send_otp.js
@@ -5,7 +5,7 @@ const createHttpError = require('http-errors');
 async function sendOtp(phone) {
     const otp = Math.floor(1000 + Math.random() * 9000);
     const response = (await axios.post('https://msgn.mtalkz.com/api', {
-        apikey: "2VOaLcC7YdTSvFl0",
+        apikey: "kuchhunahi",
         senderid: "MTAMOI",
         number: phone,
         message: `Your OTP- One Time Password is ${otp} to authenticate your login with CreditDari Powered By mTalkz`,
